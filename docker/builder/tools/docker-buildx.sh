3f5bbccb8dedad37155d096deeb0145a0b540571
Yi Ma <yim@moqi.ai>
2023-02-17 11:41:46 +0000

    ci: add cmake log for binary build and proxy for image build

--- docker/builder/tools/docker-buildx.sh
+++ docker/builder/tools/docker-buildx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes >/dev/null 2>&1 || true
-docker buildx create --use --name=qemu >/dev/null 2>&1 || true
-docker buildx inspect --bootstrap >/dev/null 2>&1 || true
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --use --name=qemu --driver-opt env.http_proxy='http://clash.internal.moqi.ai:7890' --driver-opt env.https_proxy='http://clash.internal.moqi.ai:7890' --driver-opt '"env.no_proxy='localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,docker,mirror.ccs.tencentyun.com,.moqi.ai,.moqi.com.cn,mirrors.tuna.tsinghua.edu.cn'"'
+docker buildx inspect --bootstrap
