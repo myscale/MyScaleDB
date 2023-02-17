01cb62a26e961d6d0c9b34c2c1a42f7e2a957802
shanfengp <shanfengp@moqi.ai>
2023-02-17 11:46:48 +0800

    add retry strategy for stateless test

--- docker/builder/tools/vector-search-test.sh
+++ docker/builder/tools/vector-search-test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+source /etc/profile
+arch="$(dpkg --print-architecture)"
+if [[ "x$arch" = "xamd64" ]]; then
+    FORCE_RETRY="--force-retry"
+fi
+
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PROJECT_PATH=$CUR_DIR/../../..
 WORKPATH=$PROJECT_PATH/docker/test/mqdb_run_stateless
@@ -21,7 +27,7 @@ cp -rfv tests/clickhouse-test docker/test/mqdb_run_stateless/
 docker rm -f stateless-test >/dev/null 2>&1 || true
 docker build --rm=true -t run-stateless-test docker/test/mqdb_run_stateless
 
-docker run --rm --user root --volume=$WORKPATH/test_output:/test_output --cap-add=SYS_PTRACE -e MAX_RUN_TIME=9720 -e S3_URL="https://clickhouse-datasets.s3.amazonaws.com" -e ADDITIONAL_OPTIONS="--hung-check --print-time --no-stateless --no-stateful" --name stateless-test run-stateless-test
+docker run --rm --user root --volume=$WORKPATH/test_output:/test_output --cap-add=SYS_PTRACE -e MAX_RUN_TIME=9720 -e S3_URL="https://clickhouse-datasets.s3.amazonaws.com" -e ADDITIONAL_OPTIONS="--hung-check --print-time --no-stateless --no-stateful $FORCE_RETRY" --name stateless-test run-stateless-test
 
 docker rm -f stateless-test >/dev/null 2>&1 || true
 docker rmi -f run-stateless-test >/dev/null 2>&1 || true
