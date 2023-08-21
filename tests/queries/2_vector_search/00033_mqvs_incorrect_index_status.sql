1fce846562585cab5fd950c3cd6730463e3e32c6
MochiXu <mochix@moqi.ai>
2023-08-21 15:24:35 +0800

    fix vector search test

--- tests/queries/2_vector_search/00033_mqvs_incorrect_index_status.sql
+++ tests/queries/2_vector_search/00033_mqvs_incorrect_index_status.sql
@@ -1,8 +1,9 @@
 -- Tags: no-parallel
 DROP TABLE IF EXISTS test_172_status SYNC;
-CREATE TABLE test_172_status (id UInt32, data Array(Float32), CONSTRAINT check_length CHECK length(data) = 4) ENGINE = MergeTree ORDER BY id SETTINGS min_bytes_to_build_vector_index = 0, vector_index_parameter_check=0;
+CREATE TABLE test_172_status (id UInt32, data Array(Float32), CONSTRAINT check_length CHECK length(data) = 768) ENGINE = MergeTree ORDER BY id SETTINGS min_bytes_to_build_vector_index = 0, vector_index_parameter_check=0;
 
-INSERT INTO test_172_status SELECT number, [number, number, number, number] FROM numbers(1000);
+INSERT INTO test_172_status SELECT number as id, arrayMap(x -> (rand() % 10000000) / 10000000.0 * (if(rand() % 2 = 0, 1, -1)), range(768)) as vector FROM numbers(1000);
+
 ALTER TABLE test_172_status ADD VECTOR INDEX idx data TYPE MSTG('unknown=1');
 
 SELECT sleep(2);
@@ -19,9 +20,10 @@ select table, name, expr, status from system.vector_indices where database = cur
 DROP TABLE test_172_status;
 
 DROP TABLE IF EXISTS test_172_replicated_status SYNC;
-CREATE TABLE test_172_replicated_status (id UInt32, data Array(Float32), CONSTRAINT check_length CHECK length(data) = 4) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/mqvs_00033/test_172_status', 'r1') ORDER BY id SETTINGS min_bytes_to_build_vector_index = 0, vector_index_parameter_check=0;
+CREATE TABLE test_172_replicated_status (id UInt32, data Array(Float32), CONSTRAINT check_length CHECK length(data) = 768) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/mqvs_00033/test_172_status', 'r1') ORDER BY id SETTINGS min_bytes_to_build_vector_index = 0, vector_index_parameter_check=0;
 
-INSERT INTO test_172_replicated_status SELECT number, [number, number, number, number] FROM numbers(1000);
+INSERT INTO test_172_replicated_status SELECT number as id, arrayMap(x -> (rand() % 10000000) / 10000000.0 * (if(rand() % 2 = 0, 1, -1)), range(768)) as vector FROM numbers(1000);
+
 ALTER TABLE test_172_replicated_status ADD VECTOR INDEX idx data TYPE MSTG('unknown=1');
 
 SELECT sleep(2);
