cfac25858b53dce2c912f100c9328148886d4eee
Zhuo Qiu <qiu_z@moqi.ai>
2024-01-09 15:55:37 +0800

    Fix test 00030_mqvs_skip_check_order_direction_for_distributed

--- tests/queries/2_vector_search/00030_mqvs_skip_check_order_direction_for_distributed.sql
+++ tests/queries/2_vector_search/00030_mqvs_skip_check_order_direction_for_distributed.sql
@@ -17,6 +17,7 @@ CREATE TABLE test_distributed (id UInt32, vector Array(Float32), CONSTRAINT chec
 
 SELECT 'metric_type=L2';
 ALTER TABLE test_vector_metric_type ADD VECTOR INDEX v2 vector TYPE HNSWFLAT('metric_type=L2');
+SELECT if(status='Built', sleep(0), sleep(1.99)+sleep(1.98)+sleep(1.97)+sleep(1.96)+sleep(1.95)) FROM (select status from system.vector_indices where table = 'test_vector_metric_type' and database = currentDatabase());
 
 SELECT id, distance(vector, [1.0, 1.0, 1.0]) as d FROM test_distributed order by d limit 2;
 SELECT id, distance(vector, [1.0, 1.0, 1.0]) as d FROM test_distributed order by d DESC limit 2; -- { serverError 62 }
@@ -24,6 +25,7 @@ ALTER TABLE test_vector_metric_type DROP VECTOR INDEX v2;
 
 SELECT 'metric_type=IP';
 ALTER TABLE test_vector_metric_type ADD VECTOR INDEX v2 vector TYPE HNSWFLAT('metric_type=IP');
+SELECT if(status='Built', sleep(0), sleep(1.99)+sleep(1.98)+sleep(1.97)+sleep(1.96)+sleep(1.95)) FROM (select status from system.vector_indices where table = 'test_vector_metric_type' and database = currentDatabase());
 
 SELECT id, distance(vector, [1.0, 1.0, 1.0]) as d FROM test_distributed order by d DESC limit 2;
 SELECT id, distance(vector, [1.0, 1.0, 1.0]) as d FROM test_distributed order by d limit 2; -- { serverError 62 }
