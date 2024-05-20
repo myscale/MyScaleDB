b91e41924952ef08886964b6cc7552ed75030a8f
Shanfeng Pang <shanfengp@myscale.com>
2024-05-20 06:48:24 +0000

    Enable NVMe SSD cache reuse after server restart

--- src/VectorIndex/Utils/VIUtils.cpp
+++ src/VectorIndex/Utils/VIUtils.cpp
@@ -508,23 +508,17 @@ getMergedSourcePartsFromFileName(const String & index_name, const DB::MergeTreeD
     return old_part_names;
 }
 
-String getUniqueVectorIndexCachePrefix(
+String getVectorIndexCachePrefix(
     const String & table_relative_path,
     const String & part_name,
-    const String & index_name,
-    const String & path_uuid)
+    const String & index_name)
 {
     auto global_context = DB::Context::getGlobalContextInstance();
-    String uuid_str;
-    if (path_uuid.empty())
-        uuid_str = generateUUIDv4();
-    else
-        uuid_str = path_uuid;
     if (global_context)
     {
         return fs::path(global_context->getVectorIndexCachePath())
             / fs::path(table_relative_path).parent_path().parent_path()
-            / String(cutMutVer(part_name) + "-" + index_name + "-" + uuid_str)
+            / String(cutMutVer(part_name) + "-" + index_name)
             / "";
     }
     else
