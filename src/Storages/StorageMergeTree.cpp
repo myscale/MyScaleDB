f4643869822bc6de2cfc2c09acda74e8d7d80f0a
Mochi Xu <mochix@myscale.com>
2024-08-07 02:39:00 +0000

    Optimize the QPS for TextSearch during Insert

--- src/Storages/StorageMergeTree.cpp
+++ src/Storages/StorageMergeTree.cpp
@@ -2079,7 +2079,8 @@ void StorageMergeTree::dropPart(const String & part_name, bool detach, ContextPt
                 auto metadata = part->storage.getInMemoryMetadataPtr();
                 if (metadata->hasSecondaryIndices() && metadata->getSecondaryIndices().hasFTS())
                 {
-                    TantivyIndexStoreFactory::instance().remove(part->getDataPartStoragePtr()->getRelativePath());
+                    auto index_names = metadata->getSecondaryIndices().getAllRegisteredNames();
+                    TantivyIndexStoreFactory::instance().remove(part->getDataPartStoragePtr()->getRelativePath(), index_names);
                 }
 #endif
             }
