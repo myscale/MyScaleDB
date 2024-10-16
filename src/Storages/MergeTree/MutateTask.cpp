4b1a1b03227f5d14e86445f5c8923f6ffba31a93
Jianmei Zhang <jianmeiz@moqi.ai>
2024-10-16 06:26:34 +0000

    Further optimize slow lightweight delete

--- src/Storages/MergeTree/MutateTask.cpp
+++ src/Storages/MergeTree/MutateTask.cpp
@@ -55,7 +55,7 @@ static bool checkOperationIsNotCanceled(ActionBlocker & merges_blocker, MergeLis
     return true;
 }
 
-static UInt64 getExistingRowsCount(const Block & block)
+static UInt64 getExistingRowsCount(const Block & block, bool lightweight_delete_updated, UInt64 & part_offset, std::vector<UInt64> & del_row_ids)
 {
     auto column = block.getByName(LightweightDeleteDescription::FILTER_COLUMN.name).column;
     const ColumnUInt8 * row_exists_col = typeid_cast<const ColumnUInt8 *>(column.get());
@@ -69,8 +69,18 @@ static UInt64 getExistingRowsCount(const Block & block)
     UInt64 existing_count = 0;
 
     for (UInt8 row_exists : row_exists_col->getData())
+    {
         if (row_exists)
             existing_count++;
+
+        /// Collect deleted row ids when LWD happens
+        if (lightweight_delete_updated)
+        {
+            if (!row_exists)
+                del_row_ids.emplace_back(part_offset);
+            part_offset++;
+        }
+    }
 
     return existing_count;
 }
@@ -1153,6 +1163,9 @@ private:
 
     size_t block_num = 0;
 
+    /// Used for row id when LWD needs to collect deleted row ids
+    UInt64 part_offset = 0;
+
     using ProjectionNameToItsBlocks = std::map<String, MergeTreeData::MutableDataPartsVector>;
     ProjectionNameToItsBlocks projection_parts;
     std::move_iterator<ProjectionNameToItsBlocks::iterator> projection_parts_iterator;
@@ -1192,8 +1205,10 @@ bool PartMergerWriter::mutateOriginalPartAndPrepareProjections()
 
         ctx->out->write(cur_block);
 
+        /// Try to collect deleted row ids when getting existing rows count for LWD
         if (ctx->has_lightweight_delete)
-            ctx->new_data_part->existing_rows_count += MutationHelpers::getExistingRowsCount(cur_block);
+            ctx->new_data_part->existing_rows_count += MutationHelpers::getExistingRowsCount(
+                cur_block, ctx->new_data_part->isDeletedMaskUpdated(), part_offset, ctx->new_data_part->deleted_row_ids);
 
         for (size_t i = 0, size = ctx->projections_to_build.size(); i < size; ++i)
         {
@@ -1916,7 +1931,7 @@ bool MutateTask::prepare()
         /// Support part with simple built index and decoupled part with merged old parts' built index files
         /// When any normal delete or ttl command exists, needs to be build vector index for the new data part.
         if (!ctx->need_delete_rows)
-            ctx->new_data_part->setDeletedMaskUpdate();
+            ctx->new_data_part->setDeletedMaskUpdated(true);
     }
     else
     {
