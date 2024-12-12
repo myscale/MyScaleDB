63bbeced9850f0233ef376be2a2e46ea5f68818d
Jianmei Zhang <jianmeiz@moqi.ai>
2024-12-12 14:15:46 +0800

    Add two functions for read from parts

--- src/VectorIndex/Storages/MergeTreeWithVectorScanSource.h
+++ src/VectorIndex/Storages/MergeTreeWithVectorScanSource.h
@@ -32,4 +32,33 @@ private:
     Chunk processReadResult(ChunkAndProgress chunk);
 };
 
+/// Create stream for reading parts from MergeTree.
+Pipe createReadInOrderFromPartsSource(
+    const RangesInDataParts & parts_with_ranges,
+    Names columns_to_read,
+    const StorageSnapshotPtr & storage_snapshot,
+    String log_name,
+    const PrewhereInfoPtr & prewhere_info,
+    const ExpressionActionsSettings & actions_settings,
+    const MergeTreeReaderSettings & reader_settings,
+    const MergeTreeReadTask::BlockSizeParams & block_size_params,
+    ContextPtr context,
+    size_t max_streams,
+    size_t min_marks_for_concurrent_read,
+    bool use_uncompressed_cache);
+
+/// Create stream for parallel reading single part from MergeTree.
+Pipe createReadFromPoolWithFilterFromPartSource(
+    RangesInDataParts parts_with_ranges,
+    Names columns_to_read,
+    VectorIndex::VIBitmapPtr filter,
+    const StorageSnapshotPtr & storage_snapshot,
+    String log_name,
+    const PrewhereInfoPtr & prewhere_info,
+    const ExpressionActionsSettings & actions_settings,
+    const MergeTreeReaderSettings & reader_settings,
+    const MergeTreeReadTask::BlockSizeParams & block_size_params,
+    ContextPtr context,
+    size_t max_streams,
+    size_t min_marks_for_concurrent_read);
 }
