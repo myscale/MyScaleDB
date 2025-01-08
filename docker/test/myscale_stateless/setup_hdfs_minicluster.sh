5ae921e5753acf82cdbad73dfafc1e4671ee423f
Shanfeng Pang <shanfengp@myscale.com>
2025-01-08 19:25:20 +0800

    fix some test case error

--- docker/test/myscale_stateless/setup_hdfs_minicluster.sh
+++ docker/test/myscale_stateless/setup_hdfs_minicluster.sh
@@ -10,8 +10,8 @@ cd hadoop-3.3.1
 export JAVA_HOME=/usr
 mkdir -p target/test/data
 chown clickhouse ./target/test/data
-# sudo -E -u clickhouse bin/mapred minicluster -format -nomr -nnport 12222 >> /test_output/hdfs_minicluster.log 2>&1 &
-bin/mapred minicluster -format -nomr -nnport 12222 &
+sudo -E -u clickhouse bin/mapred minicluster -format -nomr -nnport 12222 >> /test_output/hdfs_minicluster.log 2>&1 &
+# bin/mapred minicluster -format -nomr -nnport 12222 &
 
 while ! nc -z localhost 12222; do
   sleep 1
